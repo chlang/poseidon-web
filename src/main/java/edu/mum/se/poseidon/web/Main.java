024d157fe47ce7d8c42c69959ec8f2945de8c2a8
Yuriy Yugay <yuriy.yugay@bmarko.com>
2017-10-10 23:39:02 -0500

    Project created

---
+++ src/main/java/edu/mum/se/poseidon/web/Main.java
@@ -0,0 +1,17 @@
+package edu.mum.se.poseidon.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Yuriy Yugay on 10/10/2017.
+ *
+ * @author Yuriy Yugay
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main (String... args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
