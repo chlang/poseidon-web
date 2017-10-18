0b985cde69ffb1bc9422ccca70e857640d01e4aa
yuriyyu <yyugay@mum.edu>
2017-10-18 16:33:16 -0500

    Updated read and update student profile -- url has been changed Updated read and update faculty profile -- url has been changed Added CustomAuthUser.java -- to help retrieve logged user id in Controllers

--- src/main/java/edu/mum/se/poseidon/web/controllers/MainController.java
+++ src/main/java/edu/mum/se/poseidon/web/controllers/MainController.java
@@ -1,6 +1,9 @@
 package edu.mum.se.poseidon.web.controllers;
 
+import edu.mum.se.poseidon.web.CustomAuthUser;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
