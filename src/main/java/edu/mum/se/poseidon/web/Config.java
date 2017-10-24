ebd2519eb057afc443ef45bab50976a60fb15f1b
munkhuu48 <munkhuu48@gmail.com>
2017-10-23 22:15:48 -0500

    unit test implementation using Mockito

--- src/main/java/edu/mum/se/poseidon/web/Config.java
+++ src/main/java/edu/mum/se/poseidon/web/Config.java
@@ -2,10 +2,12 @@ package edu.mum.se.poseidon.web;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @Configuration
+@PropertySource(value={"classpath:application-dev.properties"})
 public class Config {
 
     @Value("${server.poseidon.url}")
---
+++ src/main/java/edu/mum/se/poseidon/web/tests/CourseControllerTest.java
@@ -0,0 +1,49 @@
+package edu.mum.se.poseidon.web.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import edu.mum.se.poseidon.web.Main;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Main.class)
+@WebAppConfiguration
+public class CourseControllerTest {
+
+	private MockMvc mockMvc;
+
+    @Autowired
+    WebApplicationContext wac;
+    
+    @Autowired
+	public UserDetailsService userDetailsService;
+    
+    @Before
+    public void setup() throws Exception{
+    	
+        // Process mock annotations
+        MockitoAnnotations.initMocks(this);
+        
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    @WithMockUser(roles={"ADMIN"})
+    public void testListCourses() throws Exception{
+        this.mockMvc.perform(get("/admin/course"))
+        						.andExpect(status().isOk());  
+    }
+}
